c155393779ce0c29e408d666e58553b216a5a56c
Eli <eli@Elis-MacBook-Air.local>
2017-10-01 15:28:03 -0700

    Initial commit - React app boilerplate and basic components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="app">
+          Github Scraper
+      </div>
+    );
+  }
+}
+
+export default App;
