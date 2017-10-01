bab4efb98fdef6b3d97a45b2329e2dc4b6bc7ffb
Eli <eli@Elis-MacBook-Air.local>
2017-10-01 16:00:06 -0700

    Build out Form and User components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './css/App.css';
+import Form from './components/Form';
 
 class App extends Component {
   render() {
     return (
       <div id="app">
-          Github Scraper
+          <Form />
       </div>
     );
   }
